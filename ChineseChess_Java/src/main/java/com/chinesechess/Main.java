49183f01290353747425378a6cf1ccbf5040cf41
DONG Newman <dongnewman2@gmail.com>
2025-11-01 17:48:25 +0800

    gradle

--- ChineseChess_Java/src/main/java/com/chinesechess/Main.java
+++ ChineseChess_Java/src/main/java/com/chinesechess/Main.java
@@ -1,12 +1,11 @@
+package com.chinesechess;
+
 import com.Controller.InitAll;
 
 public class Main {
-    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        
         InitAll.InitAllinit();
-        
     }
 }
