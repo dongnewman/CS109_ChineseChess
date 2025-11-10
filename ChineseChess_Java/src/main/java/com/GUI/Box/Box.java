d5a951ec78c45763a8c27839ec2a327e3076003a
DONG Newman <dongnewman2@gmail.com>
2025-11-10 13:56:54 +0800

    优化Box代码，去除冗余

--- ChineseChess_Java/src/main/java/com/GUI/Box/Box.java
+++ ChineseChess_Java/src/main/java/com/GUI/Box/Box.java
@@ -1,8 +1,17 @@
 package com.GUI.Box;
 
+import java.awt.image.BufferedImage;
+
 public class Box {
-    private String boxImagePath;
-    private int row;
-    private int col;
-    
+    protected static final int CELL_SIZE = 77;
+    protected static final int OFFSET_X = 39;
+    protected static final int OFFSET_Y = 55;
+    protected String file_path;
+    protected BufferedImage image;
+    protected int centerX;
+    protected int centerY;
+    protected int drawX;
+    protected int drawY;
+    protected int width;
+    protected int height;
 }
--- ChineseChess_Java/src/main/java/com/GUI/Box/RedBox.java
+++ ChineseChess_Java/src/main/java/com/GUI/Box/RedBox.java
@@ -7,55 +7,40 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class RedBox extends Box {
-    private static final int CELL_SIZE = 77;
-    private static final int OFFSET_X = 39;
-    private static final int OFFSET_Y = 55;
-
-    private String file_path = "src\\main\\resources\\Box\\RedBox.png";
-    private BufferedImage image;
-    private int centerX;
-    private int centerY;
-    private int drawX;
-    private int drawY;
-    private int width;
-    private int height;
-
     public RedBox(int col, int row) {
+        super();
+        this.file_path = "src\\main\\resources\\Box\\RedBox.png";
         try {
             File f = new File(file_path);
             if (f.exists()) {
-                image = ImageIO.read(f);
+                this.image = ImageIO.read(f);
             }
         } catch (IOException e) {
             // ignore, try classpath next
         }
-        if (image == null) {
+        if (this.image == null) {
             try (java.io.InputStream in = getClass().getResourceAsStream(file_path)) {
                 if (in != null) {
-                    image = ImageIO.read(in);
+                    this.image = ImageIO.read(in);
                 }
             } catch (IOException e) {
                 // ignore
             }
         }
-        if (image != null) {
-            width = image.getWidth();
-            height = image.getHeight();
+        if (this.image != null) {
+            this.width = this.image.getWidth();
+            this.height = this.image.getHeight();
         } else {
-            width = 0;
-            height = 0;
-            System.err.println("BlueBox: failed to load image from '" + file_path + "'.");
+            this.width = 0;
+            this.height = 0;
+            System.err.println("RedBox: failed to load image from '" + file_path + "'.");
         }
         setBoardPositionInternal(col, row);
-        // 构造后自动paint（需有Graphics对象）
-        // 如果有全局画布可用，可在此处调用 paint(g)
-        // 例如：paint(GlobalGraphicsContext.get());
-        // 如无则略过，实际项目建议在外部容器paintComponent中统一调用
     }
 
     public void paint(Graphics g) {
-        if (image != null && g != null) {
-            g.drawImage(image, drawX, drawY, null);
+        if (this.image != null && g != null) {
+            g.drawImage(this.image, this.drawX, this.drawY, null);
         }
     }
 
@@ -69,31 +54,26 @@ public class RedBox extends Box {
         setBoardPositionInternal(col, row);
     }
 
-    public int getCenterX() {
-        return centerX;
+    public int getCenterX() { 
+        return centerX; 
     }
-
-    public int getCenterY() {
-        return centerY;
+    public int getCenterY() { 
+        return centerY; 
     }
-
-    public int getWidth() {
-        return width;
+    public int getWidth() { 
+        return width; 
     }
-
-    public int getHeight() {
-        return height;
+    public int getHeight() { 
+        return height; 
     }
-
-    public BufferedImage getImage() {
-        return image;
+    public BufferedImage getImage() { 
+        return image; 
     }
-
+    
     private void updateDrawCoordinates() {
         this.drawX = centerX - width / 2;
         this.drawY = centerY - height / 2;
     }
-
     private void setBoardPositionInternal(int col, int row) {
         this.centerX = columnToX(col);
         this.centerY = rowToY(row);
@@ -103,14 +83,12 @@ public class RedBox extends Box {
     public static int columnToX(int column) {
         return (column - 1) * CELL_SIZE + OFFSET_X;
     }
-
     public static int rowToY(int row) {
         return (row - 1) * CELL_SIZE + OFFSET_Y;
     }
 
     // 判断颜色，黑色返回true
     public boolean getColor() {
-        // 约定：图片名或路径包含"black"即为黑色
         if (file_path != null && file_path.toLowerCase().contains("black")) {
             return true;
         }
